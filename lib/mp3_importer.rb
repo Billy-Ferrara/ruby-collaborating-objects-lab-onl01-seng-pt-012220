18aacd47f5fed50a100886bec30834fbb3e564c4
Billy Ferrara <willfira717@gmail.com>
2020-03-01 19:20:02 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -1,10 +1,11 @@
 class MP3Importer
 
-def initialize(name)
-  @name = name
+attr_accessor :path 
+
+def initialize(path)
+  @path = path 
 end 
 
-  def import(list_of_filenames)
-    list_of_filenames.each{|filename| Song.new_by_filename(filename)}
+  def files 
   end
 end
