31dfda14ba57e886d65fe67d890d20429edb6a55
Billy Ferrara <willfira717@gmail.com>
2020-03-01 20:34:18 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -8,8 +8,8 @@ def initialize(name)
 end
 
 def self.new_by_filename(file)
-  song_name = file.split.strip("-")[1]
-  artist= file.split.strip("-")[0]
+  song_name = file.split("-")[1]
+  artist= file.split("-")[0]
   song= self.new(song_name)
   song.artist_name = artist
   song
