fceeaa2dea2b302d41e982bdc993c2d6bef82d3f
Billy Ferrara <willfira717@gmail.com>
2020-03-01 19:51:36 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -6,4 +6,17 @@ def initialize(name)
 
 end
 
+def self.new_by_filename(file)
+  song_name = file.split(" - ")[1]
+  artist= file.split(" - ")[0]
+  song= self.new (song_name)
+  song.artist.name = artist
+  song
+end
+
+def artist_name(name)
+  self.artist= Artist.find_or_create_by_name(name)
+  self.artist.add_song(self)
+end 
+
 end
