6f0a84803d5ff659f5eec0370631d09914342323
Billy Ferrara <willfira717@gmail.com>
2020-03-01 20:47:28 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -11,7 +11,7 @@ def self.new_by_filename(file)
   song_name = file.split("-")[1]
   artist= file.split("-")[0]
   song= self.new(song_name)
-  song.artist_name = artist.split
+  song.artist_name = artist.strip("")
   artist 
   song
 
