22d38c397cb3478236cfb785ffb3e648adb24720
Billy Ferrara <willfira717@gmail.com>
2020-03-01 19:38:08 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -7,14 +7,15 @@ def initialize(path)
 end
 
   def files
+    Dir.entries(path).select {|entry| entry.include?(".mp3") }
     #only wants .mp3 files
-    @files = []
-    file = Dir.entries(@path)
-    file.each do |file|
-      if file.include?("mp3")
-        @files << file
-  end
-end
-@files 
+    #@files = []
+    #file = Dir.entries(@path)
+    #file.each do |file|
+    #  if file.include?("mp3")
+    #    @files << file
+  #end
+#end
+#@files
 end
 end
